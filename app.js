3a9dd722539be7c4efbebb85db164d6c21f67c9b
Mubashir Hassan <official.mhm13@gmail.com>
2021-07-14 02:27:44 +0500

    Added: Cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const cors = require('cors');
 const MongoStore = require('connect-mongo');
 const session = require('express-session');
 const router = require('./routes');
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV === 'production') {
   app.enable('trust proxy');
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
